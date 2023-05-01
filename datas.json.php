202d807523a82aeb020d1721acf17ec6a0b440f8
Thanachotx <120002021+Thanachotx@users.noreply.github.com>
2023-05-01 02:36:00 +0000

    update

---
+++ datas.json.php
@@ -0,0 +1,19 @@
+<?php
+require_once("database.php");
+// 63010912568
+$datas["student_code"] = "";
+$datas["student_name"] = "";
+
+if (isset($_POST['code'])) {
+    $code = $_POST['code'];
+    $sql = "select * from students where s_id ='$code'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $datas["student_code"] = $row["s_id"];
+            $datas["student_name"] = $row["fullname"];
+        }
+    }
+}
+echo json_encode($datas);
+?>
